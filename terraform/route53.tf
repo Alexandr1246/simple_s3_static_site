9a52a092464be03b3ff05855e6fc8d294f22da25
Olexandr <alex.sh1246@gmail.com>
2025-04-30 19:27:10 +0000

    add route 53

---
+++ terraform/route53.tf
@@ -0,0 +1,21 @@
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "itstep-project.online"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.itstep-project.online"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
