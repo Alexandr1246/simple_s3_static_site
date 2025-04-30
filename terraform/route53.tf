8e3cc2d9bbc66a1461474510218284b0694c33c3
Olexandr <alex.sh1246@gmail.com>
2025-04-30 19:32:22 +0000

    fix route 53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,9 +2,10 @@ resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "itstep-project.online"
   type    = "A"
+
   alias {
-    name                   = aws_cloudfront_distribution.cdn.domain_name
-    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    name                   = aws_cloudfront_distribution.static_site_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.static_site_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -13,9 +14,10 @@ resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.itstep-project.online"
   type    = "A"
+
   alias {
-    name                   = aws_cloudfront_distribution.cdn.domain_name
-    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    name                   = aws_cloudfront_distribution.static_site_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.static_site_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
