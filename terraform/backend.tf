7d90ba54761bbe44e25faa5d1b40e572c38ead67
Olexandr <alex.sh1246@gmail.com>
2025-05-02 13:51:52 +0000

    add backend file

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-0205"    # назва бакету
+    key            = "terraform/state.tfstate" # шлях до файлу стану в бакеті
+    region         = "eu-north-1"              # регіон бакету
+    encrypt        = true                      # шифрування стану
+  }
+}
---
+++ terraform/modules/s3/outputs.tf
@@ -0,0 +1,24 @@
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.static_site.bucket
+}
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.static_site.arn
+}
+
+output "bucket_id" {
+  description = "The ID of the S3 bucket"
+  value       = aws_s3_bucket.static_site.id
+}
+
+output "bucket_domain_name" {
+  description = "The domain name of the S3 bucket"
+  value       = aws_s3_bucket.static_site.website_domain
+}
+
+output "bucket_website_url" {
+  description = "The URL to access the static website"
+  value       = aws_s3_bucket.static_site.website_url
+}
