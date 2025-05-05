935804b5e41ed6b5338c7f53e944b47559dfbd90
Olexandr <alex.sh1246@gmail.com>
2025-05-05 15:51:34 +0000

    fix log_bucket_policy_3

--- terraform/modules/policy/s3_policy.tf
+++ terraform/modules/policy/s3_policy.tf
@@ -19,36 +19,37 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
 }
 
 resource "aws_s3_bucket_policy" "log_bucket_policy" {
-  bucket = var.log_bucket_name
   provider = aws.use1
+  bucket   = var.log_bucket_name
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Sid: "AWSLogDeliveryWrite",
-        Effect: "Allow",
-        Principal: {
-          Service: "delivery.logs.amazonaws.com"
-        },
-        Action: "s3:PutObject",
-        Resource: "${var.log_bucket_arn}/*",
-        Condition: {
-          StringEquals: {
-            "s3:x-amz-acl": "bucket-owner-full-control",
-            "aws:SourceAccount": var.aws_account_id
+        Sid       = "AWSLogDeliveryWrite"
+        Effect    = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action    = "s3:PutObject"
+        Resource  = "${var.log_bucket_arn}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl"      = "bucket-owner-full-control"
+            "aws:SourceAccount" = var.aws_account_id
           }
         }
       },
       {
-        Sid: "AWSLogDeliveryAclCheck",
-        Effect: "Allow",
-        Principal: {
-          Service: "delivery.logs.amazonaws.com"
-        },
-        Action: "s3:GetBucketAcl",
-        Resource: var.log_bucket_arn
+        Sid       = "AWSLogDeliveryAclCheck"
+        Effect    = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action    = "s3:GetBucketAcl"
+        Resource  = var.log_bucket_arn
       }
     ]
   })
 }
+
