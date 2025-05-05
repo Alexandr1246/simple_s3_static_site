69574c5778e196be85db7b6c6486572e0bb71680
Olexandr <alex.sh1246@gmail.com>
2025-05-05 10:32:20 +0000

    output domain name

--- terraform/modules/cloudfront/outputs.tf
+++ terraform/modules/cloudfront/outputs.tf
@@ -5,3 +5,7 @@ output "cloudfront_domain" {
 output "oai_arn" {
   value = aws_cloudfront_origin_access_identity.oai.iam_arn
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.static_site_distribution.domain_name
+}
