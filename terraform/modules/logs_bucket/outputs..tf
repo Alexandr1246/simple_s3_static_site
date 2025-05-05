eca838c0b251e6df34d52b6e223d89b2958a3f51
Olexandr <alex.sh1246@gmail.com>
2025-05-05 08:54:48 +0000

    fix logs_bucket_name   = module.logs_bucket.bucket and logs_bucket_arn    = module.logs_bucket.arn

--- terraform/modules/logs_bucket/outputs..tf
+++ terraform/modules/logs_bucket/outputs..tf
@@ -1,3 +1,10 @@
 output "bucket_name" {
   value = aws_s3_bucket.logs_bucket.bucket
 }
+output "bucket" {
+  value = aws_s3_bucket.logs_bucket.id
+}
+
+output "arn" {
+  value = aws_s3_bucket.logs_bucket.arn
+}
