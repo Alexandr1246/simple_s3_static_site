334cd4822c97f63050e6e411036fcf2397b1c707
Olexandr <alex.sh1246@gmail.com>
2025-05-05 11:18:03 +0000

    fix error

--- terraform/modules/s3/s3.tf
+++ terraform/modules/s3/s3.tf
@@ -15,12 +15,3 @@ resource "aws_s3_bucket_public_access_block" "static_site" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_s3_bucket" "logs_bucket" {
-  bucket        = var.logs_bucket_name
-
-  tags = {
-    Name        = "cloudfront-logs"
-    Environment = var.environment
-  }
-}
