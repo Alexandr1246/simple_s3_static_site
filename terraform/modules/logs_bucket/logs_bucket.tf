a43b38dbcdfb79fc71ab13e789c0426a220bf505
Olexandr <alex.sh1246@gmail.com>
2025-05-05 19:11:12 +0000

    fix access problem

--- terraform/modules/logs_bucket/logs_bucket.tf
+++ terraform/modules/logs_bucket/logs_bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "logs_bucket" {
-  provider = aws.use1
-  bucket   = var.log_bucket_name
+  provider      = aws.use1
+  bucket        = var.log_bucket_name
   force_destroy = true
 
   tags = {
@@ -8,12 +8,18 @@ resource "aws_s3_bucket" "logs_bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "logs_bucket_pab" {
-  provider = aws.use1
-  bucket   = aws_s3_bucket.logs_bucket.id
+resource "aws_s3_bucket_ownership_configuration" "logs_bucket_ownership" {
+  bucket = aws_s3_bucket.logs_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
+resource "aws_s3_bucket_public_access_block" "logs_bucket_pab" {
+  provider                  = aws.use1
+  bucket                    = aws_s3_bucket.logs_bucket.id
+  block_public_acls         = false
+  block_public_policy       = false
+  ignore_public_acls        = false
   restrict_public_buckets = false
 }
