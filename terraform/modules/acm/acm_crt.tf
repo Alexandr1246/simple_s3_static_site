f858c64f52517c7468a3c16dc6af31648e91f587
Olexandr <alex.sh1246@gmail.com>
2025-05-12 18:44:42 +0000

    add depend on

--- terraform/modules/acm/acm_crt.tf
+++ terraform/modules/acm/acm_crt.tf
@@ -32,4 +32,6 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  depends_on = [aws_acm_certificate.cert]
 }
