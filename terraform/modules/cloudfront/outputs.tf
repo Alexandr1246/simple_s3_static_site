d30a83ed32e642f54ac3c0d2eeb4e37b3f1c1ae7
Olexandr <alex.sh1246@gmail.com>
2025-05-12 19:51:38 +0000

    fix problem with validation

--- terraform/modules/cloudfront/outputs.tf
+++ terraform/modules/cloudfront/outputs.tf
@@ -1,4 +1,4 @@
-output "cloudfront_domain" {
+output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.static_site_distribution.domain_name
   description = "Доменне ім'я CloudFront Distribution"
 }
