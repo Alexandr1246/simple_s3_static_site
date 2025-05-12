6dbcce14ee3999a9479648ac61384992ebf9d9d0
Olexandr <alex.sh1246@gmail.com>
2025-05-12 13:41:15 +0000

    fix problem with main.tf_3

--- terraform/modules/route53/outputs.tf
+++ terraform/modules/route53/outputs.tf
@@ -1,3 +1,8 @@
 output "name_servers" {
   value = data.aws_route53_zone.main.name_servers
 }
+
+output "zone_id" {
+  description = "Route53 Zone ID"
+  value       = data.aws_route53_zone.main.zone_id
+}
