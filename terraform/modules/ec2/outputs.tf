9c6ac04d741f7a0c4eb81b4ee87da7dd15f99d37
Olexandr <alex.sh1246@gmail.com>
2025-05-19 09:33:48 +0000

    fix error with outputs

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,8 +1,8 @@
 output "k8s_node_public_ip" {
-  value = aws_instance.k8s_node.public_ip
+  value = aws_instance.k8s_master_node.public_ip
 }
 
 output "worker_private_ip" {
   description = "Private IP address of the worker node"
-  value       = aws_instance.k8s_worker.private_ip
+  value       = aws_instance.k8s_worker_node.private_ip
 }
