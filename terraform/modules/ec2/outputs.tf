54f27d6ff616030955134842d166bdf082600896
Olexandr <alex.sh1246@gmail.com>
2025-05-19 15:53:57 +0000

    add new ssh

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -6,3 +6,8 @@ output "worker_private_ip" {
   description = "Private IP address of the worker node"
   value       = aws_instance.k8s_worker_node.private_ip
 }
+
+output "private_key_pem" {
+  value     = tls_private_key.k8s_key.private_key_pem
+  sensitive = true
+}
