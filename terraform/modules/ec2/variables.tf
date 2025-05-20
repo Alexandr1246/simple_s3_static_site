a9d4bedd1fa97d4a0633fb15fad3795e0e308c9c
Olexandr <alex.sh1246@gmail.com>
2025-05-20 10:06:44 +0000

    fix bugs with ssh

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
   description = "Name of the existing EC2 Key Pair to use for SSH"
   type        = string
-  default = "terraform-key"
+  default = "terraform-key-ec2"
 }
