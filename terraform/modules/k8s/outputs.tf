084d65fb42af7de2c161fed86af0b7a5b96d7239
Olexandr <alex.sh1246@gmail.com>
2025-05-26 14:45:31 +0000

    remove outputs

--- terraform/modules/k8s/outputs.tf
+++ terraform/modules/k8s/outputs.tf
@@ -6,12 +6,12 @@ output "k8s_node_public_ip_worker" {
   value = aws_instance.k8s_worker_node.public_ip
 }
 
-output "worker_instance_ids" {
-  description = "EC2 Instance IDs of the worker nodes"
-  value       = module.asg_worker.autoscaling_group_instances[*].instance_id
-}
+#output "worker_instance_ids" {
+#  description = "EC2 Instance IDs of the worker nodes"
+#  value       = module.asg_worker.autoscaling_group_instances[*].instance_id
+#}
 
-output "master_instance_ids" {
-  description = "EC2 Instance IDs of the master node"
-  value       = module.asg_master.autoscaling_group_instances[*].instance_id
-}
+#output "master_instance_ids" {
+#  description = "EC2 Instance IDs of the master node"
+#  value       = module.asg_master.autoscaling_group_instances[*].instance_id
+#}
