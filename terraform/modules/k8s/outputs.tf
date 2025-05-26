39bb8278994e76ef037093e915ddc38754d74d88
Olexandr <alex.sh1246@gmail.com>
2025-05-26 14:59:27 +0000

    remove outputs

--- terraform/modules/k8s/outputs.tf
+++ terraform/modules/k8s/outputs.tf
@@ -1,10 +1,10 @@
-output "k8s_node_public_ip_master" {
-  value = aws_instance.k8s_master_node.public_ip
-}
+#output "k8s_node_public_ip_master" {
+#  value = aws_instance.k8s_master_node.public_ip
+#}
 
-output "k8s_node_public_ip_worker" {
-  value = aws_instance.k8s_worker_node.public_ip
-}
+#output "k8s_node_public_ip_worker" {
+#  value = aws_instance.k8s_worker_node.public_ip
+#}
 
 #output "worker_instance_ids" {
 #  description = "EC2 Instance IDs of the worker nodes"
