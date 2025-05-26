81affca6db511d1a98ee41a2525bb783e25db1e5
Olexandr <alex.sh1246@gmail.com>
2025-05-26 09:54:04 +0000

    remove ec2 file

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,7 +1,17 @@
-output "k8s_node_public_ip" {
+output "k8s_node_public_ip_master" {
   value = aws_instance.k8s_master_node.public_ip
 }
 
-output "worker_private_ip" {
-  value = aws_instance.k8s_worker_node.private_ip
+output "k8s_node_public_ip_worker" {
+  value = aws_instance.k8s_worker_node.public_ip
+}
+
+output "worker_instance_ids" {
+  description = "EC2 Instance IDs of the worker nodes"
+  value       = module.asg_worker.autoscaling_group_instances[*].instance_id
+}
+
+output "master_instance_ids" {
+  description = "EC2 Instance IDs of the master node"
+  value       = module.asg_master.autoscaling_group_instances[*].instance_id
 }
