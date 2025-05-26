37b26c56482687be25c7c4526869141211cb229b
Olexandr <alex.sh1246@gmail.com>
2025-05-26 19:40:23 +0000

    init destroy for asg_6

--- terraform/modules/k8s/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-0205"    # назва бакету
-    key     = "k8s-terraform/state.tfstate" # шлях до файлу стану в бакеті
-    region  = "eu-north-1"              # регіон бакету
-    encrypt = true                      # шифрування стану
-  }
-}
