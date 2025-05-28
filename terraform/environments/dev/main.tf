e8af28bcfc1cfcdf388239abb79d6cac478b4da8
Olexandr <alex.sh1246@gmail.com>
2025-05-28 19:29:53 +0000

    test k8s module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,23 +1,23 @@
-module "acm" {
-  source                    = "../../modules/acm"
-  providers                 = { aws = aws.use1 }
-  domain_name               = var.domain_name
-  subject_alternative_names = ["www.${var.domain_name}"]
-  region                    = var.region
-  zone_id                   = module.route53.zone_id
-}
+#module "acm" {
+#  source                    = "../../modules/acm"
+#  providers                 = { aws = aws.use1 }
+#  domain_name               = var.domain_name
+#  subject_alternative_names = ["www.${var.domain_name}"]
+#  region                    = var.region
+#  zone_id                   = module.route53.zone_id
+#}
 
-module "cloudfront" {
-  source                         = "../../modules/cloudfront"
-  bucket_name                    = module.s3.bucket_regional_domain_name
-  s3_origin_id                   = module.s3.bucket
-  domain_aliases                 = ["itstep-project.online", "www.itstep-project.online"]
-  acm_certificate_arn            = module.acm.acm_arn
-  log_bucket_name                = var.log_bucket_name
-  log_bucket_domain              = "${module.logs_bucket.bucket}.s3.amazonaws.com"
-  acm_certificate_validation_arn = module.acm.acm_certificate_validation_arn
+#module "cloudfront" {
+#  source                         = "../../modules/cloudfront"
+#  bucket_name                    = module.s3.bucket_regional_domain_name
+#  s3_origin_id                   = module.s3.bucket
+#  domain_aliases                 = ["itstep-project.online", "www.itstep-project.online"]
+#  acm_certificate_arn            = module.acm.acm_arn
+#  log_bucket_name                = var.log_bucket_name
+#  log_bucket_domain              = "${module.logs_bucket.bucket}.s3.amazonaws.com"
+#  acm_certificate_validation_arn = module.acm.acm_certificate_validation_arn
 
-}
+#}
 
 module "k8s" {
   source       = "../../modules/k8s"
@@ -32,19 +32,19 @@ module "logs_bucket" {
   aws_account_id  = var.aws_account_id
 }
 
-module "route53" {
-  source                    = "../../modules/route53"
-  domain_name               = var.domain_name
-  cloudfront_domain_name    = module.cloudfront.cloudfront_domain
-  cloudfront_hosted_zone_id = var.cloudfront_hosted_zone_id
-}
+#module "route53" {
+#  source                    = "../../modules/route53"
+#  domain_name               = var.domain_name
+#  cloudfront_domain_name    = module.cloudfront.cloudfront_domain
+#  cloudfront_hosted_zone_id = var.cloudfront_hosted_zone_id
+#}
 
-module "s3" {
-  source             = "../../modules/s3"
-  bucket_name        = var.bucket_name
-  environment        = var.environment
-  cloudfront_oai_arn = module.cloudfront.oai_arn
-}
+#module "s3" {
+#  source             = "../../modules/s3"
+#  bucket_name        = var.bucket_name
+#  environment        = var.environment
+#  cloudfront_oai_arn = module.cloudfront.oai_arn
+#}
 
 #module "policy" {
 #  source             = "./modules/policy"
