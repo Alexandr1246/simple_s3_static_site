4e8860de8faebe48c9ebcc093ca8b6da6a861c1b
Olexandr <alex.sh1246@gmail.com>
2025-07-06 18:34:56 +0000

    update vpc  module

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,3 +1,10 @@
 output "vpc_id" {
-  data = value.vpc.vpc_id
+  value = module.vpc.vpc_id 
+  description = "ID VPC for eks or k8s"
 }
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+  description = "ID private_subnets for eks or k8s"
+}
+
