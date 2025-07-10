d9469ad66b0fcdcf2b8daf5a931c7f3a0468f7f2
Olexandr <alex.sh1246@gmail.com>
2025-07-10 05:58:00 +0000

    remove some parametrs_1

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,10 +1,2 @@
-output "vpc_id" {
-  value = module.eks_vpc.default_vpc_id 
-  description = "ID VPC for eks or k8s"
-}
 
-output "private_subnets" {
-  value = module.eks_vpc.private_subnets
-  description = "ID private_subnets for eks or k8s"
-}
 
