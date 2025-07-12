13c8504cb75ec57c1a384a5da1646cd798cc3d30
Olexandr <alex.sh1246@gmail.com>
2025-07-12 13:28:13 +0000

    fix init vpc id_2

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -57,3 +57,15 @@ variable "ssh_key_name" {
   type        = string
   default     = "kuber-key-ec2"
 }
+
+variable "vpc_id" {
+  description = "The VPC ID for the EKS cluster"
+  type        = string
+  default     = module.eks_vpc.vpc_id
+}
+
+variable "subnet_ids" {
+  description = "Private subnet IDs for the EKS cluster"
+  type        = list(string)
+  default     = module.eks_vpc.private_subnets
+}
--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,9 +1,11 @@
 variable "vpc_id" {
   description = "The VPC ID for the EKS cluster"
   type        = string
+  default = module.eks_vpc.vpc_id
 }
 
 variable "subnet_ids" {
   description = "Private subnet IDs for the EKS cluster"
   type        = list(string)
+  default  = module.eks_vpc.private_subnets
 }
