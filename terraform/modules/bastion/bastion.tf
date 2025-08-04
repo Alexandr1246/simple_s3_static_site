b986828cea06f1d0796a0fb3cb3b7dfa09aa9bf1
Olexandr <alex.sh1246@gmail.com>
2025-08-04 10:06:34 +0000

    fix iam

--- terraform/modules/bastion/bastion.tf
+++ terraform/modules/bastion/bastion.tf
@@ -1,22 +1,3 @@
-resource "aws_iam_role" "bastion_ssm_role" {
-  name = "bastion-ssm-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.bastion_ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
 
 resource "aws_instance" "bastion" {
   ami           = var.bastion_ami_id
@@ -32,9 +13,4 @@ resource "aws_instance" "bastion" {
     Name        = "bastion-host"
     SSMManaged  = "true"
   }
-}
-
-resource "aws_iam_instance_profile" "bastion_profile" {
-  name = "bastion-ssm-profile"
-  role = aws_iam_role.bastion_ssm_role.name
 }
