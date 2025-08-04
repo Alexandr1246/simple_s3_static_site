f1380ae7d8a0b41d83c4738391d9319eaa738282
Olexandr <alex.sh1246@gmail.com>
2025-08-04 09:56:05 +0000

    add bastion ssm role

--- terraform/modules/bastion/bastion_iam.tf
+++ terraform/modules/bastion/bastion_iam.tf
@@ -18,23 +18,7 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_instance" "bastion" {
-  ami           = var.bastion_ami_id
-  instance_type = var.instance_type
-  subnet_id = var.public_subnet_ids[0]
-  vpc_security_group_ids = [var.security_group_id]
-
-  iam_instance_profile = aws_iam_instance_profile.bastion_profile.name
-
-  associate_public_ip_address = true
-
-  tags = {
-    Name        = "bastion-host"
-    SSMManaged  = "true"
-  }
-}
-
 resource "aws_iam_instance_profile" "bastion_profile" {
-  name = "bastion-ssm-profile"
+  name = "bastion-instance-profile"
   role = aws_iam_role.bastion_ssm_role.name
 }
